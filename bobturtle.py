1414b6a6fc2314194e6001ee448371c41788a71c
Cole Hadley <clh6040@psu.edu>
2022-04-26 18:01:21 -0400

    Turtle V2

--- bobturtle.py
+++ bobturtle.py
@@ -1,6 +1,6 @@
 import turtle
-#hello
-#test 2
+
+#creates the path list
 def vortexV2(multi, modulus, x, usedlist):
     path = [x]
     answer = 0
@@ -21,6 +21,7 @@ def vortexV2(multi, modulus, x, usedlist):
         i += 1
     return path
 
+#removes path positions that have been used
 def listshort(path, numlist):
     functionlist = numlist.copy()
     for i in range(len(path)):
@@ -28,58 +29,38 @@ def listshort(path, numlist):
             functionlist.remove(path[i])
     return functionlist
 
-
 def main():
     bob = turtle.Turtle()
-
     ts = bob.getscreen()
     ts.bgcolor("#c0eaf2")
 
+    #sets the multiplier, modulus, radius, and increments of the circle
     multi = int(ts.numinput("Multiplier", "Enter Multiplier: "))
     modulus = int(ts.numinput("Modulus", "Enter Modulus: "))
-    print(multi)
-    print(modulus)
-    #multi = int(input("Enter Multiplier: ")) 97
-    #modulus = int(input("Enter Modulus: ")) 168
-
-    r = 300
-    dots = modulus
-    arcAngle = 360 / dots
+    radius = 300
+    arcAngle = 360 / modulus
+    #dictionary to store positions along the circle
     dictX = {}
     dictY = {}
 
     bob.speed(0)
     bob.penup()
-    bob.setpos(0, r)
+    bob.setpos(0, radius)
     bob.pendown()
     bob.setheading(180)
-
-    for i in range(dots):
-        bob.circle(r, -arcAngle)
-        #bob.dot()
+    #draws circle and saves positons
+    for i in range(modulus):
+        bob.circle(radius, -arcAngle)
         dictX[i+1] = float(bob.xcor())
         dictY[i+1] = float(bob.ycor())
 
-    start = 1
-    usedlist = []
-    path = vortexV2(multi, modulus, start, usedlist)
-    numlist = []
-
-    for i in range(len(path)):
-        usedlist.append(path[i])
+    usedlist = []   #keeps track of path values used
+    numlist = []    #keep track of numbers not used yet
     for i in range(modulus - 1):
         numlist.append(i + 1)
 
-    bob.penup()
-    bob.setpos(dictX[path[0]], dictY[path[0]])
-    bob.pendown()
-    for i in range(1, len(path) + 1):
-        bob.setpos(dictX[path[i - 1]], dictY[path[i - 1]])
-
+    #creates all the loops in the vortex
     while len(numlist) != 0:
-        numlist = listshort(path, numlist)
-        if len(numlist) == 0:
-            break
         start = numlist[0]
         path = vortexV2(multi, modulus, start, usedlist)
         for i in range(len(path)):
@@ -89,6 +70,7 @@ def main():
         bob.pendown()
         for i in range(1, len(path) + 1):
             bob.setpos(dictX[path[i - 1]], dictY[path[i - 1]])
+        numlist = listshort(path, numlist)
 
     bob.hideturtle()
     #ts.getcanvas().postscript(file="vortex.eps")
